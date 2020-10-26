3c51ef0348a9da66edcddc810a3c9a22e1e734c2
Andrew <andrew.casin@gmail.com>
2020-10-26 00:29:30 -0500

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,8 @@
 def square_array(array)
+  new_array = []
   # your code here
+  array.each do |numbers|
+    new_array << numbers**2 #shovel in the squared value of each array number
+  end
+  new_array #need to return new array
 end
